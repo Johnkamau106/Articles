1d4be631cbe19ce35640627247d76a0b2a9959c6
John Kamau <johnkamau106@example.com>
2025-05-26 15:48:19 +0300

    Add Article class methods for CRUD operations and enhance database interactions

--- code-challenge/lib/models/article.py
+++ code-challenge/lib/models/article.py
@@ -4,3 +4,58 @@ class Article:
         self.title = title
         self.author_id = author_id
         self.magazine_id = magazine_id
+
+
+    def save(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        if self.id:
+            cursor.execute("""
+                UPDATE articles 
+                SET title = ?, author_id = ?, magazine_id = ? 
+                WHERE id = ?
+            """, (self.title, self.author_id, self.magazine_id, self.id))
+        else:
+            cursor.execute("""
+                INSERT INTO articles (title, author_id, magazine_id) 
+                VALUES (?, ?, ?)
+            """, (self.title, self.author_id, self.magazine_id))
+            self.id = cursor.lastrowid
+            conn.commit()
+            conn.close()
+
+    def delete(self):
+        conn = get_connection()
+        cursor + conn.cursor()
+        cursor.execute("DELETE FROM article where id = ?", (self.id,))
+        conn.commit()
+        conn.close()
+
+    @classmethod
+    def find_by_id(cls, id):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM articles WHERE id = ?", (id,))
+        row = cursor.fetchone()
+        conn.close()
+        if row:
+            return cls(row['title'], row['author_id'], row['magazine_id'], row['id'])
+        return None
+
+    @classmethod
+    def find_by_title(cls, title):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM articles WHERE title = ?", (title,))
+        rows = cursor.fetchall()
+        conn.close()
+        return [cls(row['title'], row['author_id'], row['magazine_id'], row['id']) for row in rows]
+
+    @classmethod
+    def find_by_author(cls, author_id):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM articles WHERE author_id = ?", (author_id,))
+        rows = cursor.fetchall()
+        conn.close()
+        return [cls(row['title'], row['author_id'], row['magazine_id'], row['id']) for row in rows]
